fb7c363de653e06353fd4678026200d3c7b19d3a
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-12-24 11:51:46 +0200

    Update task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
-console.log("Hello")
+const bodyEl = document.querySelector("body");
+const buttonStart = document.querySelector("button[data-start]");
+const buttonStop = document.querySelector("button[data-stop]");
+
+bodyEl.style.textAlign = "center";
+buttonStart.style.padding = "15px 30px";
+buttonStart.style.fontSize = "20px";
+buttonStart.style.backgroundColor = "white";
+buttonStop.style.padding = "15px 30px";
+buttonStop.style.fontSize = "20px";
+buttonStop.style.backgroundColor = "white";
+
+buttonStop.setAttribute("disabled", "true");
+let timerId = null;
+
+buttonStart.addEventListener("click", handleClickStart);
+buttonStop.addEventListener("click", handleClickStop);
+
+
+function handleClickStart() {
+    if(!bodyEl.hasAttribute("background-color")) {
+        timerId = setInterval(() => {
+            bodyEl.style.backgroundColor = getRandomHexColor()
+        }, 1000);
+        buttonStart.setAttribute("disabled", "true");
+        buttonStop.removeAttribute("disabled");
+         
+    } else {
+        clearInterval(timerId)
+    }
+}
+
+function handleClickStop() {
+    buttonStart.removeAttribute("disabled");
+    buttonStop.setAttribute("disabled", "true");
+    clearInterval(timerId);
+
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
