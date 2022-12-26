f29f7b7f26fa07333572ccef791830b8244fd54c
KonstantinYevchuk <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-12-26 11:44:15 +0000

    Deploying to gh-pages from @ KonstantinYevchuk/goit-js-hw-09@ebac00998b5e520f91c900f0c35b644a16e66835 🚀

---
+++ 03-promises.9ea0282a.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".form");document.querySelector("button").addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.reset()}))}();
+//# sourceMappingURL=03-promises.9ea0282a.js.map
---
+++ 03-promises.ecc341b2.js
@@ -0,0 +1,2 @@
+document.querySelector(".form");document.querySelector("button").addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.reset()}));
+//# sourceMappingURL=03-promises.ecc341b2.js.map
