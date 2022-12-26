ebac00998b5e520f91c900f0c35b644a16e66835
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-12-26 13:43:49 +0200

    update task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+const formEl = document.querySelector(".form")
+const buttonEl = document.querySelector("button");
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve('Hello')
+      } else {
+        reject('Erorr')
+      }
+    }, 2000)
+  })
+  promise.then(value => {console.log(value)}).catch(erorr => {console.log(erorr);})
 }
+
+
+
+
+
+buttonEl.addEventListener("submit", onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault()
+  evt.currentTarget.reset()
+   
+}
+
+
+
+
+// const inputDelay = document.querySelector("input[name='delay']");
+// console.log(inputDelay);
+// const inputStep = document.querySelector("input[name='step']");
+// console.log(inputStep);
+// const inputAmount = document.querySelector("input[name='amount']");
+// console.log(inputAmount);
